a1a3c3b09349fb8ac2abb5d5db76e7eecb1d9b3d
Lucas Trautmann <lucastrautmann1@gmail.com>
2022-08-10 17:12:56 +0200

    Created a code using the buttons and the lights

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tricky_exercice
 I learn how to use the ESP32 with leds and buttons
+
+info:
+- leds not lighting up altogether: don't forget the alimentation of the card
+  
+To fix:
+- if called in a separate function, the leds blink instead of staying on
--- test1/src/main.cpp
+++ test1/src/main.cpp
@@ -1,36 +1,71 @@
+/*
+ This code lights the two led strips on white
+ When a button is pressed, it lights the strips with the corresponding color,
+ until a new color is pressed.
+ */
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h> // used to light the led strips
 
-int led1_pin = 12;
-int led2_pin = 13;
+#define LED1_PIN 12 // pin of the first led
+#define LED2_PIN 13 
 
-Adafruit_NeoPixel strip_led1(10, led1_pin, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip_led2(10, led2_pin, NEO_GRB + NEO_KHZ800);
+#define BUTTON_R_PIN 34 // pin of the red button
+#define BUTTON_G_PIN 35
+#define BUTTON_B_PIN 25
+#define BUTTON_Y_PIN 26
+
+int nb_leds_strip = 10; // number of leds in each strip
+Adafruit_NeoPixel strip_led1(nb_leds_strip, LED1_PIN, NEO_GRB + NEO_KHZ800); // first led strip
+Adafruit_NeoPixel strip_led2(nb_leds_strip, LED2_PIN, NEO_GRB + NEO_KHZ800); // second led strip
+
+// initial color
+uint32_t color = strip_led1.Color(255, 255, 255); // here white
 
 void setup() {
 
-  strip_led1.begin();
-  strip_led2.begin();
   Serial.begin(115200);
   Serial.println("Reset happened");
+  strip_led1.begin(); // Configure NeoPixel pin of the first strip for output
+  strip_led2.begin();
 
-}
+  // initialize the pushbutton pins as an pull-up inputs
+  pinMode(BUTTON_R_PIN, INPUT_PULLUP); //INPUT_PULLUP : 0 if button pressed, else 1 
+  pinMode(BUTTON_G_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_B_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_Y_PIN, INPUT_PULLUP);
+
+} 
 
 void loop() {
+  // defining the colors (would be better to do it outside the loop)
+  uint32_t red = strip_led1.Color(255, 0, 0);
+  uint32_t green = strip_led1.Color(0, 255, 0);
+  uint32_t blue = strip_led1.Color(0, 0, 255);
+  uint32_t yellow = strip_led1.Color(255, 180, 0);
 
-// light every led of the the led strips
-  for(int i = 0; i < 10; i++){
-    strip_led1.setBrightness(200);
+  if(!digitalRead(BUTTON_R_PIN)) color = red;
+  if(!digitalRead(BUTTON_G_PIN)) color = green;
+  if(!digitalRead(BUTTON_B_PIN)) color = blue;
+  if(!digitalRead(BUTTON_Y_PIN)) color = yellow;
+
+  for(int i = 0; i < 10; i++){ // for every led of the strips
+    strip_led1.setBrightness(200); // total brightness of the leds
     strip_led2.setBrightness(200);
 
-    strip_led1.clear();
+    strip_led1.clear(); // Fill the whole NeoPixel strip with black.
     strip_led2.clear();
 
-    strip_led1.setPixelColor(i, 255, 190, 0);
-    strip_led2.setPixelColor(i, 255, 190, 0);
+    strip_led1.setPixelColor(i, color); 
+    strip_led2.setPixelColor(i, color);
 
-    strip_led1.show();
+    strip_led1.show(); // refresh all the leds
     strip_led2.show();
   }
   
+  // debug :
+  // Serial.println(digitalRead(BUTTON_R_PIN));
+  // Serial.println(digitalRead(BUTTON_G_PIN));
+  // Serial.println(digitalRead(BUTTON_B_PIN));
+  // Serial.println(digitalRead(BUTTON_Y_PIN));
+
 }
